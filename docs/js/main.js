4619f50c68698080291cff37e7f5ddf9ec1b1940
MelikaZln <mohamad.abdollahipour@gmail.com>
2024-03-16 18:51:03 +0330

    Projektet är klart

--- docs/js/main.js
+++ docs/js/main.js
@@ -160,4 +160,66 @@ function sortTable(e) {
     return e[t].localeCompare(a[t]);
   }), updateTable();
 }
+function searchCity() {
+  var e = document.getElementById("cityInput").value,
+    t = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: 0,
+        lng: 0
+      },
+      zoom: 10
+    }),
+    a = {
+      query: e,
+      fields: ["name", "geometry"]
+    };
+  new google.maps.places.PlacesService(t).findPlaceFromQuery(a, function (e, a) {
+    if (a === google.maps.places.PlacesServiceStatus.OK) {
+      t.setCenter(e[0].geometry.location);
+      new google.maps.Marker({
+        map: t,
+        position: e[0].geometry.location
+      });
+    }
+  });
+}
+function getTimezone() {
+  var e = document.getElementById("cityInput").value;
+  fetch("https://maps.googleapis.com/maps/api/geocode/json?address=".concat(e, "&key=AIzaSyCyzJpyMvzQrkMx2Nhb39itNxmBMfRbw64")).then(function (e) {
+    return e.json();
+  }).then(function (t) {
+    if ("ZERO_RESULTS" !== t.status) {
+      var a = t.results[0].geometry.location.lat,
+        n = t.results[0].geometry.location.lng;
+      fetch("https://maps.googleapis.com/maps/api/timezone/json?location=".concat(a, ",").concat(n, "&timestamp=").concat(Date.now() / 1e3, "&key=AIzaSyCyzJpyMvzQrkMx2Nhb39itNxmBMfRbw64")).then(function (e) {
+        return e.json();
+      }).then(function (t) {
+        var a = t.timeZoneId,
+          n = t.timeZoneName;
+        document.getElementById("timezone-info").innerText = "Tidszon f\xF6r ".concat(e, ": ").concat(n, " (").concat(a, ")");
+      })["catch"](function (e) {
+        console.error("Fel vid hämtning av tidszon:", e);
+      });
+    } else document.getElementById("timezone-info").innerText = "Staden, ".concat(e, ", hittades inte!");
+  })["catch"](function (t) {
+    console.error("Fel vid hämtning av stadskoordinater:", t), document.getElementById("timezone-info").innerText = "Staden ".concat(e, " hittades inte!");
+  });
+}
+function getWeather() {
+  var e = document.getElementById("cityInput").value;
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=".concat(e, "&appid=cd57e09d6d65c8196f784c17892e4f50")).then(function (e) {
+    return e.json();
+  }).then(function (e) {
+    var t = document.getElementById("weather-info");
+    if (200 === e.cod) {
+      var a = Math.round(e.main.temp - 273.15),
+        n = e.weather[0].description,
+        o = e.wind.speed,
+        r = e.main.humidity;
+      t.innerHTML = "<p><strong>Väder:</strong> " + n + "</p><p><strong>Temperatur:</strong> " + a + "°C</p><p><strong>Vindhastighet:</strong> " + o + " m/s</p><p><strong>Fuktighet:</strong> " + r + "%</p>";
+    } else t.innerHTML = "Ingen väderinformation hittades för den sökta staden";
+  })["catch"](function (e) {
+    console.error("Error:", e);
+  });
+}
 searchInput.addEventListener("input", updateTable), fetchSchedule();
